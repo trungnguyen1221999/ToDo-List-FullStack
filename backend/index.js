c7a71fb891129bbd44492a6ac33ea7f75343e49e
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-16 19:52:30 +0300

    deploy

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./DB/ConnectDB.js";
 import { taskRouter } from "./routes/taskRoutes.js";
 import cors from "cors";
-import path from 'path'
+import path from "path";
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
@@ -11,11 +11,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 if (process.env.NODE_ENV !== "production") {
-  app.use(cors({
-    origin: "*", // hoặc "*" để cho phép tất cả
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+  app.use(
+    cors({
+      origin: "*", // hoặc "*" để cho phép tất cả
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 }
 // Connect to database
 connectDB();
@@ -23,14 +25,16 @@ app.use("/task", taskRouter);
 
 // app.use("/task", taskRoutes);
 
-if (process.env.NODE_ENV = "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(
     express.static(path.join(__dirname, "../frotnend/vite-project/dist"))
   );
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frotnend/vite-project/dist/index.html"))
-  })
+  app.get(/.*/, (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frotnend/vite-project/dist/index.html")
+    );
+  });
 }
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
